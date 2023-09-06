46218f9dd1d9c2275bb5cf28f19591febc75edf3
Aaron Nguyen <me@aaronnguyen.dev>
2023-09-06 10:12:10 -0400

    Generate via Spring Initializr

---
+++ src/main/java/com/team23/geektext/GeekTextApplication.java
@@ -0,0 +1,13 @@
+package com.team23.geektext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekTextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekTextApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/team23/geektext/healthcheck/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.team23.geektext.healthcheck;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+	@GetMapping("/health")
+	public String checkHealth() {
+		return "Application is healthy!";
+	}
+}
