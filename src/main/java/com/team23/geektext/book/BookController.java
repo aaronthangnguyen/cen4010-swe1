4bbcaaf7b29c506898d560f2c50a93f15591aab3
Aaron Nguyen <me@aaronnguyen.dev>
2023-09-26 15:07:53 -0400

    Add `create new book`

--- src/main/java/com/team23/geektext/book/BookController.java
+++ src/main/java/com/team23/geektext/book/BookController.java
@@ -1,9 +1,13 @@
 package com.team23.geektext.book;
 
+import com.team23.geektext.exception.AuthorNotFoundException;
 import java.util.List;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +24,24 @@ public class BookController {
     public ResponseEntity<List<Book>> getAllBooks() {
         List<Book> books = bookService.getAllBooks();
         return new ResponseEntity<List<Book>>(books, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createNewBook(@RequestBody Book book) {
+        try {
+            Book savedBook = bookService.createNewBook(book);
+            String responseMessage = "Book '" + savedBook.getName() + "' successfully created.";
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(responseMessage);
+        } catch (AuthorNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body("An unexpected error occurred.");
+        }
     }
 }
--- src/main/java/com/team23/geektext/book/BookService.java
+++ src/main/java/com/team23/geektext/book/BookService.java
@@ -1,5 +1,7 @@
 package com.team23.geektext.book;
 
+import com.team23.geektext.exception.AuthorNotFoundException;
+import com.team23.geektext.repository.AuthorRepository;
 import com.team23.geektext.repository.BookRepository;
 import java.util.List;
 import org.springframework.stereotype.Service;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class BookService {
     private BookRepository bookRepository;
+    private AuthorRepository authorRepository;
 
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
@@ -14,5 +17,13 @@ public class BookService {
 
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
+    }
+
+    public Book createNewBook(Book book) {
+        if (!authorRepository.existsById(book.getAuthorId())) {
+            throw new AuthorNotFoundException(
+                    "The author with ID '" + book.getAuthorId() + "' does not exist.");
+        }
+        return bookRepository.save(book);
     }
 }
