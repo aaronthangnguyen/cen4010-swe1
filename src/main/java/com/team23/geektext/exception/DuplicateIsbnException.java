38a14b2fd1b45333959f4127a2ede063d47d7502
Aaron Nguyen <37777232+aaronthangnguyen@users.noreply.github.com>
2023-09-27 15:12:04 -0400

    Add `create new book` endpoint (#23)
    * Add `create new book`
    
    * Add tests
    
    * Handle duplicate ISBN

---
+++ src/main/java/com/team23/geektext/exception/DuplicateIsbnException.java
@@ -0,0 +1,7 @@
+package com.team23.geektext.exception;
+
+public class DuplicateIsbnException extends RuntimeException {
+    public DuplicateIsbnException(String message) {
+        super(message);
+    }
+}
