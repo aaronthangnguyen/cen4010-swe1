927d10ae2db245e13b738e5712d83c441b790e5c
murphybrenna <143561836+murphybrenna@users.noreply.github.com>
2023-10-12 21:16:33 -0400

    Changed rating to double and added the ability to get average rating of a book

--- src/main/java/com/team23/geektext/repository/BookRatingRepository.java
+++ src/main/java/com/team23/geektext/repository/BookRatingRepository.java
@@ -2,6 +2,13 @@ package com.team23.geektext.repository;
 
 import com.team23.geektext.BookRating.BookRating;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 public interface BookRatingRepository extends JpaRepository<BookRating, Long> {
+
+    List<BookRating> findByBookID(Long bookID); //gets ratings for one book
+
+    @Query("SELECT AVG(r.rating) FROM BookRating r WHERE r.bookID = ?1")
+    Double findAverageRatingByBookID(Long bookID);
 }
